2d7a74b40a7bfb1768fa9cf071cdb49b3e2eef74
meetbhuva07 <meetbhuva0009@gmail.com>
2026-05-02 11:19:38 +0530

    i lern variable and input faction

---
+++ chapter 0/Assignment.py
@@ -0,0 +1,5 @@
+# Assignment Question : Print your Name 10 times using * symbol and each time it should be on next line .
+
+print (("hello, meet" + "\n ") * 10) 
+
+
---
+++ chapter 1/input.py
@@ -0,0 +1,7 @@
+print ("Simple use input faction : ")
+
+name = input ("Enter your name :")
+age = input ("Enter your age :")
+
+print("Your Name is",name)
+print("Your age is",age)
