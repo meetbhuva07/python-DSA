140dd3bdd37845174583c5fe6c0f4794d2788db8
meetbhuva07 <meetbhuva0009@gmail.com>
2026-05-04 19:47:16 +0530

    chapter 1 complted

---
+++ chapter 1/pyAssignment2.py
@@ -0,0 +1,9 @@
+# Question - take diameter as input and calculate the area of a cricle..
+
+
+diameter = int(input("Enter your diameter : "))
+radius = diameter/2
+area = 3.14 * (radius ** 2)
+
+print("Radius of cricle is : ",radius)
+print("Area of cricle is : ",area)
