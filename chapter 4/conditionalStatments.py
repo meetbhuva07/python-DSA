78d4786efacfd751186440bbdb84b95920bb8a4d
meetbhuva07 <meetbhuva0009@gmail.com>
2026-05-21 19:26:23 +0530

    complted chapter 4

---
+++ chapter 4/conditionalStatments.py
@@ -0,0 +1,15 @@
+# conditional statments
+ 
+marks = 95 
+
+if( marks >= 90):
+    print("Grade A")
+elif(marks >= 80):
+    print("Grade B")
+
+Age = 20
+
+if(Age >=   18 ) :
+    print("You are eligible to vote")
+else :
+    print("You are not eligible to vote")
---
+++ chapter 4/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+marks = 95 
+
+if( marks >= 90):
+    print("Grade A")
+elif(marks >= 80):
+    print("Grade B")
